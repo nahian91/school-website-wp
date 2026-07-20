ccbc8626de5d193be623f7562b59f86daf2c4c86
nahian28 <nahiansylhet@gmail.com>
2026-07-20 19:33:25 +0600

    init

--- templates-page/template-gallery.php
+++ templates-page/template-gallery.php
@@ -97,14 +97,6 @@ get_header(); ?>
                         <div class="dnt-gallery-info"><div class="dnt-gallery-title">বিজ্ঞান মেলা প্রদর্শন</div><div class="dnt-gallery-tag">বিজ্ঞান</div></div>
                     </div>
 
-                    <!-- আইটেম ৬ -->
-                    <div class="dnt-gallery-item">
-                        <div class="dnt-gallery-thumb">
-                            <img src="https://images.unsplash.com/photo-1518605368461-1d3ce08ce006?q=80&w=800&auto=format&fit=crop" alt="Gallery Image 6">
-                            <div class="dnt-gallery-overlay"><a href="#"><svg viewBox="0 0 24 24" fill="#fff"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg></a></div>
-                        </div>
-                        <div class="dnt-gallery-info"><div class="dnt-gallery-title">ফুটবল টুর্নামেন্ট ফাইনাল</div><div class="dnt-gallery-tag">ক্রীড়া</div></div>
-                    </div>
 
                     <!-- আইটেম ৭ -->
                     <div class="dnt-gallery-item">
